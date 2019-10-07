8e563e33c454fe8124294d9f655a79815f6c2fc0
Moshe <guitaristmo@gmail.com>
2019-10-07 00:33:30 -0400

    GuiImplemented now takes advantage of another class which takes care of swing alone. GuiImplemented manages the mainFrame and this other Swing class. Progam is running great - except for the progress bar which is still glitching
    I forgot to include the new Swing class in the previous commit

---
+++ src/main/java/edu/touro/mco152/bm/SwingWorkerImplemented.java
@@ -0,0 +1,78 @@
+package edu.touro.mco152.bm;
+
+import javafx.beans.binding.ObjectExpression;
+
+import javax.swing.*;
+import java.util.List;
+
+public class SwingWorkerImplemented extends SwingWorker <Boolean, DiskMark>
+{
+    private MyDiskWorker worker = null;
+    private MyApp mainApp = null;
+    private GuiImplemented mainGui;
+
+    public SwingWorkerImplemented(GuiImplemented mainGui, MyApp mainApp)
+    {
+        super();
+        this.mainGui = mainGui;
+        this.mainApp = mainApp;
+    }
+
+    public void setWorker(MyDiskWorker worker){this.worker = worker;}
+
+    void uSetProgress(int progress){setProgress(progress);}
+
+    void uPublish(Object... marks)
+    {
+        //I would use the following line but it wasn't casting properly
+        //publish((DiskMark[]) marks);
+
+        //so this is my version and it works
+        DiskMark[] mark = new DiskMark[marks.length];
+        for (int index = 0; index < marks.length; index++)
+            mark[index] = (DiskMark) marks[index];
+        publish(mark);
+    }
+
+    @Override
+    protected Boolean doInBackground() throws Exception { return worker.runBenchmark(); }
+
+    @Override
+    public void process(List<DiskMark> markList) {
+        markList.stream().forEach( (m) -> {
+            if (m.type==DiskMark.MarkType.WRITE) {
+                mainGui.addWriteMark(m);
+            } else {
+                mainGui.addReadMark(m);
+            }
+        });
+    }
+
+    @Override
+    public void done() {
+        System.out.println("display.done was called");
+        if (mainApp.autoRemoveData) {
+            Util.deleteDirectory(mainApp.dataDir);
+        }
+        mainApp.state = MyApp.State.IDLE_STATE;
+        mainGui.adjustSensitivity();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
